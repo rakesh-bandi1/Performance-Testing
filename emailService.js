2c68fad7a1362435472a75d357a1d26f982d4b10
rakesh-bandi1 <rakesh.bandi@tellius.com>
2025-10-06 17:31:37 +0530

    Added direct API Login

--- emailService.js
+++ emailService.js
@@ -20,7 +20,7 @@ class EmailService {
       subject: config.subject || 'Vizpad Performance Test Results',
       
       // Optional settings
-      cc: config.cc || process.env.EMAIL_CC || 'ankur.gollen@tellius.com',
+      cc: config.cc || process.env.EMAIL_CC || '',
       bcc: config.bcc || process.env.EMAIL_BCC || '',
       
       ...config
